818800edd5b38545ab520d2e1a009387e13e2a54
zhik <zhikhe@gmail.com>
2020-01-20 19:38:52 -0500

    init svelte

---
+++ client/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import 'bulma/css/bulma.css'
+  import { Field, Input } from 'svelma'
+  export let name
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+
+  <form action="">
+    <Field label="Name">
+      <Input type="text" value="test" placeholder="Text input" />
+    </Field>
+  </form>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
