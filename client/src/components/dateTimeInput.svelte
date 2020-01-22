8bec83105dadf127d729af64d08949ca52f4b8bd
zhik <zhikhe@gmail.com>
2020-01-22 10:56:53 -0500

    added datetime input

---
+++ client/src/components/dateTimeInput.svelte
@@ -0,0 +1,32 @@
+<style>
+  #input-date {
+    margin-right: 1em;
+  }
+</style>
+
+<script>
+  import { Field } from 'svelma'
+  import dateFormat from '../utils/dateFormat'
+
+  export let datetime
+  export let initDateTime = new Date()
+  export let id
+  export let label
+
+  let date = initDateTime ? dateFormat(initDateTime, 'yyyy-mm-dd') : undefined
+  let time = initDateTime ? dateFormat(initDateTime, 'hh:MM') : undefined
+
+  $: {
+    if (date && time) {
+      datetime = `${date}T${time}:00.000Z`
+    } else {
+      datetime = null
+    }
+  }
+</script>
+
+<label for="{id}" class="{id} label">{label}</label>
+<Field grouped>
+  <input id="input-date" min="2000-01-01" type="date" bind:value="{date}" aria-label="{label}" />
+  <input id="input-time" type="time" bind:value="{time}" aria-label="{label}" />
+</Field>
--- client/src/layouts/new.svelte
+++ client/src/layouts/new.svelte
@@ -1,2 +1,88 @@
+<style>
+  form {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .panel-left {
+    flex: 1 1 auto;
+    min-width: 30em;
+    margin-right: 1em;
+  }
+
+  .panel-right {
+    margin-top: 1em;
+    flex: 1 1 auto;
+    min-width: 20em;
+    height: 30em;
+  }
+
+  textarea {
+    width: 30em;
+    height: 5em;
+  }
+</style>
+
+<script>
+  import { Field, Input } from 'svelma'
+  import DateTimeInput from '../components/dateTimeInput.svelte'
+  import Leaflet from '../components/leaflet.svelte'
+
+  //todo- fix label for - a11y for svelma
+  let statuses = ['Open', 'Closed']
+
+  let form = {
+    Status: statuses[0],
+    'Request/Issue': '',
+    'Date Opened': null,
+    'Initial Note': ''
+  }
+
+  $: console.log(form)
+
+  async function handleSubmit() {
+    console.log('submitted')
+    //redirect to issue page
+  }
+</script>
+
 <h3 class="title is-3">Create Request</h3>
 <h6 class="subtitle is-6">Add a new request to the database</h6>
+
+<form on:submit|preventDefault="{handleSubmit}">
+
+  <div class="panel-left">
+    <Field label="Status">
+      <div class="control">
+        <div class="select">
+          <select bind:value="{form['Status']}">
+            {#each statuses as status}
+              <option value="{status}">{status}</option>
+            {/each}
+          </select>
+        </div>
+      </div>
+    </Field>
+    <DateTimeInput label="Date Opened" id="date-opened" bind:datetime="{form['Date Opened']}" />
+    <Field label="Request/Issue" for="request-issue">
+      <textarea
+        id="request-issue"
+        name="request-issue-text"
+        class="textarea"
+        bind:value="{form['Request/Issue']}"
+      ></textarea>
+    </Field>
+    <Field label="Initial Note" for="initial-note">
+      <textarea
+        id="initial-note"
+        name="initial-note-text"
+        class="textarea"
+        bind:value="{form['Initial Note']}"
+      ></textarea>
+    </Field>
+  </div>
+  <div class="panel-right">
+    <Leaflet />
+
+  </div>
+</form>
